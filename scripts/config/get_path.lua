a44b3af1cf09b1afdc25c318239d851f844a02c4
Evan Gipson <evangipson@gmail.com>
2025-02-17 20:24:40 -0600

    Format path from get_path

--- scripts/config/get_path.lua
+++ scripts/config/get_path.lua
@@ -1,10 +1,13 @@
+local string_gsub = string.gsub
 local app_settings = require(INCLUDE_PATH() .. '\\app_settings')
 
 local function get_path()
     if app_settings.path == nil then
         return "Could not find path to scripts."
     end
-    return app_settings.path
+
+    local formatted_path = string_gsub(app_settings.path, '\\\\', '\\')
+    return formatted_path
 end
 
 return get_path
