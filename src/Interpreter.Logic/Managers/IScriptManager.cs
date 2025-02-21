687a82310e9b91ac4d730145f2e663d812012f73
Evan Gipson <evangipson@gmail.com>
2025-02-20 20:20:50 -0600

    Add generic-typed Run/RunAsync in ScriptManager

--- src/Interpreter.Logic/Managers/IScriptManager.cs
+++ src/Interpreter.Logic/Managers/IScriptManager.cs
@@ -22,9 +22,28 @@ public interface IScriptManager
     dynamic? Run(string scriptRelativePath, IEnumerable<string>? arguments = null);
 
     /// <summary>
+    /// Runs the script that lives at <paramref name="scriptRelativePath"/>, and passes along the
+    /// <paramref name="arguments"/> that are provided, then captures and returns the
+    /// <typeparamref name="TResult"/> result.
+    /// </summary>
+    /// <typeparam name="TResult">
+    /// The type of result from running the script.
+    /// </typeparam>
+    /// <param name="scriptRelativePath">
+    /// The path to the script to run, relative to the "ScriptSettings:Path" application setting.
+    /// </param>
+    /// <param name="arguments">
+    /// An optional collection of <see langword="string"/> arguments to provide the script.
+    /// </param>
+    /// <returns>
+    /// A <typeparamref name="TResult"/> script result, defaults to <see langword="null"/>.
+    /// </returns>
+    TResult? Run<TResult>(string scriptRelativePath, IEnumerable<string>? arguments = null);
+
+    /// <summary>
     /// Runs the script that lives at <paramref name="scriptRelativePath"/> asynchronously, and
     /// passes along the <paramref name="arguments"/> that are provided, then captures and returns
-    /// the result as <see cref="Task"/> with a <see langword="dynamic"/> type.
+    /// the result as a <see cref="Task"/> with a <see langword="dynamic"/> type.
     /// </summary>
     /// <param name="scriptRelativePath">
     /// The path to the script to run, relative to the "ScriptSettings:Path" application setting.
@@ -36,4 +55,23 @@ public interface IScriptManager
     /// A <see cref="Task"/> filled with a <see langword="dynamic"/> script result, defaults to <see langword="null"/>.
     /// </returns>
     Task<dynamic?> RunAsync(string scriptRelativePath, IEnumerable<string>? arguments = null);
+
+    /// <summary>
+    /// Runs the script that lives at <paramref name="scriptRelativePath"/> asynchronously, and
+    /// passes along the <paramref name="arguments"/> that are provided, then captures and returns
+    /// the result as a <see cref="Task"/> filled with the <typeparamref name="TResult"/> result.
+    /// </summary>
+    /// <typeparam name="TResult">
+    /// The type of result from running the script.
+    /// </typeparam>
+    /// <param name="scriptRelativePath">
+    /// The path to the script to run, relative to the "ScriptSettings:Path" application setting.
+    /// </param>
+    /// <param name="arguments">
+    /// An optional collection of <see langword="string"/> arguments to provide the script.
+    /// </param>
+    /// <returns>
+    /// A <see cref="Task"/> filled with a <typeparamref name="TResult"/> script result, defaults to <see langword="null"/>.
+    /// </returns>
+    Task<TResult?> RunAsync<TResult>(string scriptRelativePath, IEnumerable<string>? arguments = null);
 }
